07dc096373dbbff0e188f663a98099d83ea8a4e3
Weslley Rocha <48062297+WESLLEYROCHA87@users.noreply.github.com>
2022-01-09 02:16:12 -0300

    Leia nome e imprima na tela
    Programa que lê o nome de uma pessoa e mostre uma mensagem de boas vindas;

---
+++ PythonExercicios/ex002.py
@@ -0,0 +1,5 @@
+# https://www.youtube.com/watch?v=FNqdV5Zb_5Q&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=8
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas;
+
+nome = input('Digite seu nome: ')
+print('Seja bem vindo(a), {}!'.format(nome))
