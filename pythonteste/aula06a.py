7c258082507bb01207e4056d69e7a437cc6b71d1
Weslley Rocha <48062297+WESLLEYROCHA87@users.noreply.github.com>
2022-01-09 02:36:41 -0300

    Tipos Primitivos e Saída de Dados
    # isnumeric -> Ele é um número;
    # isalpha -> Ele é letra;
    # isalphanu, -> Ele é alfa numérico;
    # isupper -> Somente com maisculas;
    # islower-> Ele está em minúsculos;

---
+++ pythonteste/aula06a.py
@@ -0,0 +1,9 @@
+# https://www.youtube.com/watch?v=hdDHg1p3YVc&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=9
+
+# Tipos Primitivos e Saída de Dados
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite o segundo valor: '))
+soma = n1 + n2
+print('A soma entre {} e {} vale: {}'.format(n1, n2, soma))
+
---
+++ pythonteste/aula06b.py
@@ -0,0 +1,12 @@
+# https://www.youtube.com/watch?v=hdDHg1p3YVc&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=9
+
+# Métodos de teste de tipo
+
+# isnumeric -> Ele é um número;
+# isalpha -> Ele é letra;
+# isalphanu, -> Ele é alfa numérico;
+# isupper -> Somente com maisculas;
+# islower-> Ele está em minúsculos;
+
+n = float(input('Digite um valor: '))
+print(n)
