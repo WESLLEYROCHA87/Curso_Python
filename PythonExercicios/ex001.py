cc6e541ba08963411ccd53d56384b9902967e6ed
Weslley Rocha <48062297+WESLLEYROCHA87@users.noreply.github.com>
2022-01-09 02:09:13 -0300

    Preparação e impressão da primeira mensagem
    Preparação do ambiente!
    Impressão da primeira mensagem na tela através do console.;
    Criação de variável para a apresentação da mensagem;

---
+++ PythonExercicios/ex001.py
@@ -0,0 +1,14 @@
+# https://www.youtube.com/watch?v=nIHq1MtJaKs&list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0&index=7
+# Crie um programa que escreva "Olá Mundo" na tela.
+
+# 1 Exemplo:
+#print('Olá Mundo!')
+
+# 2 Exemplo:
+# Criando uma variável para a apresentação da mensagem
+
+msg = 'Olá Mundo!'
+print('Olá Mundo!')
+
+
+
